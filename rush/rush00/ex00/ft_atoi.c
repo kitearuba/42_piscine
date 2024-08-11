296f8dd282bb6f5ec27b4b92cee771b682646721
Christian <kitearuba@gmail.com>
2024-08-11 15:22:17 +0200

    1108-1521 Uploaded version of main to take arguments from command line for Rush00

---
+++ rush/rush00/ex00/ft_atoi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrrodri <chrrodri@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 20:48:43 by chrrodri          #+#    #+#             */
+/*   Updated: 2024/06/28 15:16:32 by chrrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+    int	sign;
+    int	nbr;
+
+    sign = 1;
+    nbr = 0;
+    while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
+        nptr++;
+    if (*nptr == '-' || *nptr == '+')
+    {
+        if (*nptr == '-')
+            sign = -1;
+        nptr++;
+    }
+    while (*nptr >= '0' && *nptr <= '9')
+    {
+        nbr = (nbr * 10) + (*nptr - '0');
+        nptr++;
+    }
+    return (sign * nbr);
+}
--- rush/rush00/ex00/main.c
+++ rush/rush00/ex00/main.c
@@ -13,9 +13,25 @@
 #include <unistd.h>
 
 void	rush(int x, int y);
+int	ft_atoi(const char *nptr);
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	rush(5, 0);
-	return (0);
+    int	x;
+    int	y;
+
+    if (argc != 3)
+    {
+        write(2, "Error: Please provide exactly 2 arguments.\n", 43);
+        return (1);
+    }
+    x = ft_atoi(argv[1]);
+    y = ft_atoi(argv[2]);
+    if (x <= 0 || y <= 0)
+    {
+        write(2, "Error: Arguments must be positive integers.\n", 44);
+        return (1);
+    }
+    rush(x, y);
+    return (0);
 }
